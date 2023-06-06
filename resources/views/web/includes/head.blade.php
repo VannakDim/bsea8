472f6284710581ee91852216e0c66908b4a095da
VannakDim <hannah@Vannaks-MacBook-Pro.local>
2023-06-06 10:34:21 +0700

    test title with screen

--- resources/views/web/includes/head.blade.php
+++ resources/views/web/includes/head.blade.php
@@ -2,7 +2,7 @@
 <title>@yield('title') - {{ $setting->website_title }}</title>
 <link rel="shortcut icon"  href="{{ asset('web/favicon/favicon.png') }}" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1" />
-<meta name="author" content="ClusterCoding <clustercoding@gmail.com>" />
+<meta name="author" content="ProgrammerKonKhmer <vannakhannah@gmail.com>" />
 <meta name="keywords" content="@yield('keywords')" />
 <meta name="description" content="@yield('description')" />
 <!-- CSS Files -->
