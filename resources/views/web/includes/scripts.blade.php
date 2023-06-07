57c670630fe84ef24bf218cab8c838466fb079c4
VannakDim <hannah@Vannaks-MacBook-Pro.local>
2023-06-07 22:08:02 +0700

    error found

--- resources/views/web/includes/scripts.blade.php
+++ resources/views/web/includes/scripts.blade.php
@@ -1,11 +1,11 @@
 <script type="text/javascript" src="{{ asset('web/js/jquery.js') }}"></script>
 <script type="text/javascript" src="{{ asset('web/js/bootstrap.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('web/js/owl.carousel.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('web/js/owl.carousel.js') }}"></script>
 <!-- <script type="text/javascript" src="{{ asset('web/js/html5lightbox.js') }}"></script> -->
 <!-- <script type="text/javascript" src="{{ asset('web/js/clock.js') }}"></script> -->
 <script type="text/javascript" src="{{ asset('web/js/jquery.bootstrap.newsbox.min.js') }}"></script>
 <script src="{{ asset('web/js/script.js') }}"></script>
-<script type="text/javascript" src="{{ asset('web/js/parsley.min.js') }}"></script>
+<!-- <script type="text/javascript" src="{{ asset('web/js/parsley.min.js') }}"></script> -->
 
 <script type="text/javascript">
 	/** store subscriber **/
