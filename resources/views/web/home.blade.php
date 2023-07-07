3ca1c494d532433f4afbbfcdaac3a6af90472a95
Vannak Dim <admin@macs-MacBook-Pro.local>
2023-07-07 07:46:24 +0700

    old banner

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -17,7 +17,7 @@
         <img src="{{ get_banner_image_url($banner->image) }}" alt="{{ $banner->title }}" class="img-responsive" style="margin-top: 80px;">
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <div class="panel panel-info" style="box-shadow: 0 8px 61px -2px rgba(0,0,0,.2);margin-top: -42px;">
                         <div class="panel-body">
                             <h3 class="text-primary" style="margin-top: 0px; color: #008fd5"><strong>{{ $banner->title }}</strong></h3>
