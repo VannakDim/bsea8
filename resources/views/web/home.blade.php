85f9eadd95a38602d5a3aadb87cee4e7f9f8eb0a
Vannak Dim <admin@macs-MacBook-Pro.local>
2023-07-07 07:50:16 +0700

    old banner

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -18,9 +18,9 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="panel panel-info" style="box-shadow: 0 8px 61px -2px rgba(0,0,0,.2);margin-top: -42px;">
+                    <div class="panel panel-info" style="box-shadow: 0 8px 61px -2px rgba(0,0,0,.2);margin-top: -20px;">
                         <div class="panel-body">
-                            <h3 class="text-primary" style="margin-top: 0px; color: #008fd5"><strong>{{ $banner->title }}</strong></h3>
+                            <h4 class="text-primary text-center" style="margin-top: 0px; color: #008fd5"><strong>{{ $banner->title }}</strong></h4>
                         </div>
                     </div>
                 </div>
