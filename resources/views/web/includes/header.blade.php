936b9504c0e8123d02c029a140118e2df907ed2a
Vannak Dim <vannakhannah@gmail.com>
2023-12-04 16:12:26 +0700

    Re-design Navbar

--- resources/views/web/includes/header.blade.php
+++ resources/views/web/includes/header.blade.php
@@ -65,7 +65,7 @@
     <nav class="nav">
       <div class="nav__data">
           <a href="{{ route('homePage') }}" class="nav__logo">
-          <img id="logo-img" src="{{ asset('web/logo/bsea-logo.png') }}" alt=""​><div class="bsea-title"><h4>សមាគមកាបូប​ ស្បែកជើង និងអេឡិចត្រូនិចកម្ពុជា</h4>Cambodia's Bag, Shoe & Electronic Association</div>
+          <img id="logo-img" src="{{ asset('web/logo/bsea-logo.png') }}" alt=""​><div class="bsea-title"><h4> សមាគមកាបូប​ ស្បែកជើង និងអេឡិចត្រូនិចកម្ពុជា</h4> Cambodia's Bag, Shoe & Electronic Association</div>
           </a>
           
           <div class="nav__toggle" id="nav-toggle">
