d988f3e76ee3e8e7fd77b0bb254dd82913967e75
Vannak Dim <vannakhannah@gmail.com>
2023-12-08 23:52:01 +0700

    edit member view

--- resources/views/admin/member/index.blade.php
+++ resources/views/admin/member/index.blade.php
@@ -3,8 +3,6 @@
 
 
 @section('style')
-<!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
-	<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css"> -->
 
 	<link rel="stylesheet" href="{{ asset('admin/datatable/css/dataTables.bootstrap.min.css')}}" />
 	<link rel="stylesheet" href="{{ asset('admin/datatable/css/buttons.dataTables.min.css')}}">
@@ -53,10 +51,7 @@
 								<th>Workers</th>
 								<th>Telephone</th>
 								<th>Created By</th>
-								{{-- <th>Created At</th> --}}
-								{{-- <th>Updated At</th> --}}
-								{{-- <th width="10%">Publication Status</th> --}}
-								<th width="7%">Action</th>
+								<th width="50px">Action</th>
 							</tr>
 						</thead>
 					</table>
@@ -69,59 +64,6 @@
 	</div>
 </section>
 <!-- /.main content -->
-
-<!-- add member modal -->
-<div id="add-modal" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-	
-	<!-- view banner modal -->
-	<div id="view-modal" class="modal fade bs-example-modal-lg print-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-		<div class="modal-dialog modal-lg" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<div class="btn-group pull-right no-print">
-						<div class="btn-group">
-							<button class="tip btn btn-default btn-flat btn-sm" id="print-button" data-toggle="tooltip" data-original-title="Print">
-								<i class="fa fa-print"></i>
-								<span class="hidden-sm hidden-xs"></span>
-							</button>
-						</div>
-						<div class="btn-group">
-							<button class="tip btn btn-default btn-flat btn-sm" data-toggle="tooltip" data-original-title="Close" data-dismiss="modal" aria-label="Close">
-								<i class="fa fa-remove"></i>
-								<span class="hidden-sm hidden-xs"></span>
-							</button>
-						</div>
-					</div>
-					<h4 class="modal-title" id="view-title"></h4>
-				</div>
-				<div class="modal-body">
-					<table class="table table-bordered table-striped">
-						<tbody>
-							<tr>
-								<td colspan="2"><img src="" id="view-image" class="img-responsive img-thumbnail img-rounded" width="100%"></td>
-							</tr>
-							<tr>
-								<td>URL</td>
-								<td id="view-url"></td>
-							</tr>
-							<tr>
-								<td>Created By</td>
-								<td id="view-created-by"></td>
-							</tr>
-							<tr>
-								<td>Publication Status</td>
-								<td id="view-publication-status"></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div class="modal-footer no-print">
-					<button type="button" class="btn btn-default btn-flat" data-dismiss="modal" aria-label="Close">Close</button>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- /.view banner modal -->
 
 	<!-- delete banner modal -->
 	<div id="delete-modal" class="modal modal-danger fade" id="modal-danger">
@@ -152,7 +94,7 @@
 			<!-- /.modal-dialog -->
 		</div>
 		<!-- /.delete banner modal -->
-	</div>
+	
 
 	@endsection
 
@@ -185,48 +127,6 @@
 		});
 
 
-
-		/** Add **/
-		$(".add-button").click(function(){
-			$('#add-modal').modal('show');
-		});
-
-		// /** Store **/
-		$("#store-button").click(function(){
-			var postData = new FormData($("#banner_add_form")[0]);
-			$( '#title-error' ).html( "" );
-			$( '#url-error' ).html( "" );
-			$( '#image-error' ).html( "" );
-			$( '#publication-status-error' ).html( "" );
-			$.ajax({
-				type:'POST',
-				url:'{{ route('admin.members.store') }}',
-				processData: false,
-				contentType: false,
-				data : postData,
-				success:function(data) {
-					console.log(data);
-					if(data.errors) {
-						if(data.errors.title){
-							$( '#title-error' ).html( data.errors.title[0] );
-						}
-						if(data.errors.url){
-							$( '#url-error' ).html( data.errors.url[0] );
-						}
-						if(data.errors.image){
-							$( '#image-error' ).html( data.errors.image[0] );
-						}
-						if(data.errors.publication_status){
-							$( '#publication-status-error' ).html( data.errors.publication_status[0] );
-						}
-					}
-					if(data.success) {
-						window.location.href = '{{ route('admin.members.index') }}';
-					}
-				},
-			});
-		});
-
 		/** Get datatable **/
 		function get_table_data(){
 			$('#members-table').DataTable({
@@ -254,9 +154,6 @@
 				{data: 'number_of_worker'},
 				{data: 'telephone'}, 
 				{data: 'username', name: 'username', orderable: true, searchable: true},
-				// {data: 'created_at'},
-				// {data: 'updated_at'},
-				// {data: 'publication_status', name: 'publication_status', orderable: false, searchable: false},
 				{data: 'action', name: 'action', orderable: false, searchable: false},
 				],
 				// order: [[0, 'desc']],
